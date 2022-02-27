7a982d11e9a2ff2667607df52bc9cd68aeeaf0a8
unknown <lanntd14@gmail.com>
2022-02-27 23:21:59 +0700

    finished 2

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "lanntd14 finished");
+		get("/hello", (req, res) -> "lanntd14 finished 22222");
 	}
 }
